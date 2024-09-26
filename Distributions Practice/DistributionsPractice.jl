a3bb6c7a57e0414121429fcbde2bbf9570e68e3e
Matthew Lee Loftus <MatthewLeeLoftus@users.noreply.github.com>
2024-09-26 01:56:30 -0600

    Added CairoMakie Practice

--- Distributions Practice/DistributionsPractice.jl
+++ Distributions Practice/DistributionsPractice.jl
@@ -1,8 +1,8 @@
 # Distributions Practice
 
-using Distributions
+using Distributions 
 
-using Random, Distributions
+using Random, Distributions 
 
 Random.seed!(123) 
 
@@ -22,13 +22,11 @@ y = rand(Normal(1,2), 100)
 
 mean(y)
 
-Binomial(100, 0.6)
+Binomial(100, 0.6) 
 
 Cauchy(1000, 15)
 
 Multinomial(10, 2)
-
-Wishart(1, 2)
 
 fieldnames(Wishart)
 
@@ -52,4 +50,36 @@ std(f)
 
 # Messing around with Distributions
 
+# Histograms
+
+using CairoMakie 
+
+seconds = 0:0.1:2
+measurements = [8.2, 8.4, 6.3, 9.5, 9.1, 10.5, 8.6, 8.2, 10.5, 8.5, 7.2,
+        8.8, 9.7, 10.8, 12.5, 11.6, 12.1, 12.1, 15.1, 14.7, 13.1]
+
+
+lines(seconds, measurements)
+
+measurements2 = rand(z, 21)
+
+lines(seconds, measurements2)
+
+scatter(seconds, measurements)
+
+scatter(seconds, measurements2)
+
+c = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+scatter(seconds, measurements2 + measurements)
+
+lines(seconds, measurements + measurements2 + c)
+
+m = exp(measurements2*c')
+
+lines(seconds, exp.(seconds) .+ 7)
+
+scatter(seconds, measurements)
+lines!(seconds, exp.(seconds) .+ 7)
+current_figure()
 
